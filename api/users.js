958c4e21d79a9139eae2ae4a3ea17226d91f35ba
Pedro Martin <pmargom@gmail.com>
2016-06-24 13:31:30 +0000

    asas

--- api/users.js
+++ api/users.js
@@ -1,6 +1,13 @@
 
 module.exports = {
     "get": function (req, res, next) {
-        return res.json({resultado: "ok"});
+       var query = {
+          sql: 'SELECT email, firstName, lastName, photoUrl FROM users",
+          parameters: []
+      };
+      req.azureMobile.data.execute(query)
+      .then(function (results) {
+          res.json({ users: results });
+      });
     }
 }
