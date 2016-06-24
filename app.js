2203eea03fcd3f9ffaadc544649f4a44a1c0d75d
Pedro Martin <pmargom@gmail.com>
2016-06-24 13:34:08 +0000

    asas

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
 
+// This is a base-level Azure Mobile App SDK.
+var express = require('express'),
+    azureMobileApps = require('azure-mobile-apps'),
+    bodyParser = require('body-parser'),
+    usersApi = require('./api/users'),
+    loginApi = require('./api/login');
+
+// Set up a standard Express app
+var app = express();
+
+var mobile = azureMobileApps(); 
+app.use(mobile);    // Register the Azure Mobile Apps middleware
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use('/api/v1/users', usersApi());
+app.use('/api/v1/login', loginApi());
+
+
+app.listen(process.env.PORT || 3000);   // Listen for requests
+/*
 // ----------------------------------------------------------------------------
 // Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
@@ -36,3 +61,4 @@ mobile.tables.initialize()
         app.listen(process.env.PORT || 3000);   // Listen for requests
     });
 
+*/
