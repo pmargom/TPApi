745bad507987d5d0956d847198f1ff1ad002ede7
Pedro Martin <pmargom@gmail.com>
2016-06-28 12:05:03 +0000

    Commit for merging

--- app.js
+++ app.js
@@ -7,8 +7,57 @@
 var express = require('express'),
     azureMobileApps = require('azure-mobile-apps');
 
+var argv = require('minimist')(process.argv.slice(2));
+var swagger = require("swagger-node-express");
+var bodyParser = require( 'body-parser' );
+
 // Set up a standard Express app
 var app = express();
+var subpath = express();
+
+app.use(bodyParser());
+app.use("/v1", subpath);
+
+swagger.setAppHandler(subpath);
+
+app.use(express.static('dist'));
+
+swagger.setApiInfo({
+    title: "example API",
+    description: "API to do something, manage something...",
+    termsOfServiceUrl: "",
+    contact: "yourname@something.com",
+    license: "",
+    licenseUrl: ""
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+// Set api-doc path
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+// Configure the API domain
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".')
+
+// Configure the API port
+var port = 8080;
+if(argv.port !== undefined)
+    port = argv.port;
+else
+    console.log('No --port=xxx specified, taking default port ' + port + '.')
+
+// Set and display the application URL
+var applicationUrl = 'http://' + domain + ':' + port;
+console.log('snapJob API running on ' + applicationUrl);
+
+
+swagger.configure(applicationUrl, '1.0.0');
 
 // If you are producing a combined Web + Mobile app, then you should handle
 // anything like logging, registering middleware, etc. here
